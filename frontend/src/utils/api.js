a1f0777eccddf22c8b6bede10be991bcb0b12670
Ashidu Dissanayake <157984013+ashiduDissanayake@users.noreply.github.com>
2025-04-07 18:29:43 +0530

    updated the AI image

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,7 +3,7 @@ const token = localStorage.getItem("token");
 
 // Create an Axios instance with the base URL of the backend
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Adjust this if your backend runs on a different port or domain
+  baseURL: process.env.NEXT_PUBLIC_URL, // Adjust this if your backend runs on a different port or domain
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${token}`
